1f721038fb54d43541d90c27dd46e0692ba4bf75
victor de souza magaldi <victmagaldi@hotmail.com>
2022-06-11 22:21:29 -0300

    return testings in firebase

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App";
 import App2 from "./App-copy";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* app 2 Auth banco de dados  */}
     <App2 />
   </React.StrictMode>,
