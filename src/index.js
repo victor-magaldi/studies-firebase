1c536f207e58b4b01655fa6c7031eefee3613d75
victor de souza magaldi <victmagaldi@hotmail.com>
2022-06-11 22:43:25 -0300

    theme provider React bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 // import App2 from "./App-copy";
 import App from "./App";
@@ -7,10 +8,13 @@ import { Login } from "./pages/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    {/* app 2 Auth banco de dados  */}
-    {/* <App2 /> */}
-    <Login />
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+    >
+      <App />;{/* app 2 Auth banco de dados  */}
+      {/* <App2 /> */}
+      <Login />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
