3881aa145605b6334ba81b865ee23abdb4c14dd4
victor de souza magaldi <victmagaldi@hotmail.com>
2022-06-11 22:40:23 -0300

    clear project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 
 // import App2 from "./App-copy";
 import App from "./App";
+import { Login } from "./pages/Login";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     {/* app 2 Auth banco de dados  */}
     {/* <App2 /> */}
+    <Login />
   </React.StrictMode>,
   document.getElementById("root")
 );
