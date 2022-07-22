2d7b8c073fb7ecdd18e3c8a313de4bf3bf734d7c
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-22 19:33:11 -0300

    form

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -13,16 +13,16 @@ export function FormLogin() {
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>E-mail</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" name="email" />
+        <Form.Control type="email" placeholder="email" name="email" />
         <Form.Text className="text-muted">Não poderá ser alterado</Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>senha</Form.Label>
-        <Form.Control type="password" placeholder="Password" name="password" />
+        <Form.Control type="password" placeholder="senha" name="password" />
       </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        Registrar
       </Button>
     </Form>
   );
